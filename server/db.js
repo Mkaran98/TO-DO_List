a68690045005e9998074e7ed84c3455e90d8f3bf
Mkaran <marotirahangdale98@gmail.com>
2025-12-05 12:13:27 +0530

    connect database

---
+++ server/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+async function connectToDB(params) {
+  try {
+    await mongoose.connect(process.env.MONGO_URL)
+  } catch (error) {
+    console.log(error)
+  }
+}
--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 import express from "express";
+import dotenv, { config } from "dotenv";
+dotenv.config()
+const app = express();
 
-const app = express()
+const port = process.env.PORT || 3000;
 
-const port = 3000
-
-
-app.listen(port,()=>{
-  console.log(`server is running on ${port}`)
-})
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
